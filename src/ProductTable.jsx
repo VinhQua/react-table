ec04dff2fd3d3ac0e3e587bdae0b56b2078fa7f3
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-04 12:41:56 +0900

    Add filtering

--- src/ProductTable.jsx
+++ src/ProductTable.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import React from 'react'
 import { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
-import { useTable } from 'react-table';
+import { useSortBy, useTable } from 'react-table';
 export function ProductTable(props){
     const [products,setProducts]=useState([]);
     function capitalizeFirstLetter(string) {
@@ -20,6 +20,13 @@ export function ProductTable(props){
     }
     const productData = useMemo(()=>[...products],[products]);
     const productColumn =   useMemo(()=> products[0]? Object.keys(products[0]).filter(key=>key !== 'rating').map(key=> {
+      if (key==='image'){
+        return{
+          Header:capitalizeFirstLetter(key),
+          accessor:key,
+          Cell: ({value})=> <img className='product-image' src={value} alt="" />
+        }
+      }
       return { Header:capitalizeFirstLetter(key), accessor:key}
     }) : [],[products])
   
@@ -27,7 +34,21 @@ export function ProductTable(props){
         fetchProduct();
      
       },[]) 
-    const tableInstance = useTable({columns:productColumn,data:productData})
+    const tableHooks =(hooks)=>{
+      hooks.visibleColumns.push((columns) => [
+        ...columns,
+        {
+          id:'Edit',
+          Header:'Edit',
+          Cell:({row})=>(
+            <button onClick={()=> alert('Editing ' + row.values.title)}>Edit</button>
+          )
+        }
+      ])
+    }
+    const tableInstance = useTable({columns:productColumn,data:productData}
+    ,tableHooks,
+    useSortBy,  )
     const {
         getTableProps,
         getTableBodyProps,
@@ -35,6 +56,7 @@ export function ProductTable(props){
         rows,
         prepareRow,
       } = tableInstance  
+      const isEven = (index) =>{index % 2===0}
     return(
         <div className='container'>
             <table {...getTableProps()}>
@@ -46,9 +68,10 @@ export function ProductTable(props){
            {// Loop over the headers in each row
            headerGroup.headers.map(column => (
              // Apply the header cell props
-             <th {...column.getHeaderProps()}>
+             <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                {// Render the header
                column.render('Header')}
+               {column.isSorted ? (column.isSortedDesc? " ⇓": " ⇑"):""}
              </th>
            ))}
          </tr>
@@ -57,12 +80,12 @@ export function ProductTable(props){
      {/* Apply the table body props */}
      <tbody {...getTableBodyProps()}>
        {// Loop over the table rows
-       rows.map(row => {
+       rows.map((row,index) => {
          // Prepare the row for display
          prepareRow(row)
          return (
            // Apply the row props
-           <tr {...row.getRowProps()}>
+           <tr {...row.getRowProps()} className={isEven(index)?"bg-green":""}>
              {// Loop over the rows cells
              row.cells.map(cell => {
                // Apply the cell props
